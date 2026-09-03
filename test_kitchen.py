482b9f144abb31a8d9a60dd7e8a9895ea3798177
Mononokaaa <new.nichanan2@gmail.com>
2026-09-03 12:06:01 +0700

    feat: add test multiplication

--- test_kitchen.py
+++ test_kitchen.py
@@ -2,13 +2,14 @@ from kitchen import Quantity
 
 def test_multiplication():
     flour = Quantity(200)
-    flour.times(3)
-    assert flour.amount == 600
+    triple_flour = flour.times(3)
+    assert triple_flour.amount == 600
+    assert flour.amount == 200  # Original remains unchanged
 
 def test_multiplication_by_two():
     flour = Quantity(200)
-    flour.times(2)
-    assert flour.amount == 400
+    double_flour = flour.times(2)
+    assert double_flour.amount == 400
 
 def test_multiplication_returns_a_new_quantity():
     flour = Quantity(200)
